88bd15f7e73495fe479dade3af17170cc6c868eb
RyanWood2334 <ryanwooduwcoding.2334@gmail.com>
2023-04-24 22:20:58 -0700

    created functions for creating engineers and interns

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const inquirer = require("inquirer");
-const employee = require("./lib/Employee");
-const engineer = require("./lib/Engineer");
-const intern = require("./lib/Intern");
-const manager = require("./lib/Manager");
+const Employee = require("./lib/Employee");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
 const fs = require("fs");
 const { type } = require("os");
 
@@ -11,7 +11,7 @@ const team = [];
 //need a function for create manager
 const createTeamManager = () => {
   inquirer
-    .createPromptModule([
+    .prompt([
       {
         type: "input",
         name: "tmName",
@@ -20,7 +20,7 @@ const createTeamManager = () => {
       {
         type: "input",
         name: "tmId",
-        message: "What is your Team Manager's employee ID??",
+        message: "What is your Team Manager's employee ID?",
       },
       {
         type: "input",
@@ -36,7 +36,7 @@ const createTeamManager = () => {
     .then((ans) => {
       const manager = new Manager(
         ans.tmName,
-        ans.tmID,
+        ans.tmId,
         ans.tmEmail,
         ans.tmOffice
       );
@@ -48,7 +48,7 @@ const createTeamManager = () => {
 
 const addTeammate = () => {
   inquirer
-    .createPromptModule([
+    .prompt([
       {
         type: "list",
         name: "addteammate",
@@ -56,15 +56,99 @@ const addTeammate = () => {
         choices: [
           "Add an engineer",
           "Add an intern",
-          "finish building your team",
+          "Finish building your team",
         ],
       },
     ])
     .then((ans) => {
+      switch (ans.addTeammate) {
+        case "Add an engineer":
+          createEngineer();
+          break;
+        case "Add an intern":
+          createIntern();
+          break;
+        case "Finish building your team":
+          console.log(team);
+      }
+
       console.log(ans);
     });
 };
 //need a function for add engineer
+const createEngineer = () => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "engineerName",
+        message: "What is your Engineer's name?",
+      },
+      {
+        type: "input",
+        name: "engineerId",
+        message: "What is your Engineer's employee ID??",
+      },
+      {
+        type: "input",
+        name: "engineerEmail",
+        message: "What is your Engineer's email address?",
+      },
+      {
+        type: "input",
+        name: "engineerGithub",
+        message: "What is your Engineer's github profile?",
+      },
+    ])
+    .then((ans) => {
+      const engineer = new Engineer(
+        ans.engineerName,
+        ans.engineerID,
+        ans.engineerEmail,
+        ans.engineerGithub
+      );
+      team.push(engineer);
+      console.table(team);
+      addTeammate();
+    });
+};
+
+const createIntern = () => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "internName",
+        message: "What is your Intern's name?",
+      },
+      {
+        type: "input",
+        name: "internId",
+        message: "What is your Intern's employee ID??",
+      },
+      {
+        type: "input",
+        name: "internEmail",
+        message: "What is your Intern's email address?",
+      },
+      {
+        type: "input",
+        name: "internSchool",
+        message: "What school is your intern attending?",
+      },
+    ])
+    .then((ans) => {
+      const intern = new Intern(
+        ans.internName,
+        ans.internID,
+        ans.internEmail,
+        ans.internGithub
+      );
+      team.push(intern);
+      console.table(team);
+      addTeammate();
+    });
+};
 //need a function for add intern
 //need a function to close recursion
 //need a function to
